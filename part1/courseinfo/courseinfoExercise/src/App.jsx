bbb3aa4b69b7de070aab8eb4a5c63cf04dea1b7a
dinningway <avalonstar05@gmail.com>
2023-07-23 04:56:32 -0400

    Uploaded 1.3-1.4: course information step3 and 4, need to put components in a separate folder for better readability. Do tomorrow...

--- part1/courseinfo/courseinfoExercise/src/App.jsx
+++ part1/courseinfo/courseinfoExercise/src/App.jsx
@@ -3,6 +3,7 @@
 //Total renders the total number of exercises.
 
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -12,21 +13,32 @@ const Header = (props) => {
 
 {/*1.2: course information, step2, creating a Part component*/}
 const Part = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>{props.parts} {props.exercises}</p>
+      <p>{props.parts} {props.exercises}</p> {/* This is "prop consumption" */}
     </div>
   )
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
+    <div>
+      <Part parts={props.parts[0].name} exercises={props.parts[0].exercises} /> {/* This is "prop assignment" */}
+      <Part parts={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part parts={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+  
+  //Commenting out old part 1a code
+  /*return (
     <div>
       <Part parts={props.part1} exercises={props.exercises1} />
       <Part parts={props.part2} exercises={props.exercises2} />
       <Part parts={props.part3} exercises={props.exercises3} />
     </div>
-  )
+  )*/
 }
 
 {/* Commenting out the 1.1 Content components */}
@@ -38,41 +50,67 @@ const Content = (props) => {
   )
 }*/
 
-const Total = ({exercises}) => {
-  let sum = 0;
-  for (let i = 0; i < exercises.length; i++) {
-    sum += numbers[i];
-  }
+const Total = (props) => {
+  console.log(props)
+  
+  let totalSum = 0
+  
+  props.parts.forEach((part) => {
+    totalSum += part.exercises
+  })
 
+  return (
+    <div>
+      <p>Number of exercises {totalSum}</p>
+    </div>
+  )
+}
+
+/* One way of doing it where I directly use the exercises var
+const Total = ({exercises}) => {
   return (
     <div>
       <p>Number of exercises {exercises}</p>
     </div>
   )
 }
+*/
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },  
+
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   
   return (
     <div>
       <Header course={course} />
+      <Content parts={parts}/>
+      <Total parts={parts}/>
       {/* Commenting out the 1.1 Content components */}
       {/*<Content parts={part1} exercises={exercises1}/>
       <Content parts={part2} exercises={exercises2}/>
       <Content parts={part3} exercises={exercises3}/>*/}
-      <Content
+      {/* Commenting out the 1.2 Content and Total components */}
+      {/*<Content
       part1={part1} exercises1={exercises1}
       part2={part2} exercises2={exercises2}
       part3={part3} exercises3={exercises3}
       />
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+    <Total exercises={exercises1 + exercises2 + exercises3}/>*/}
     </div>
   )
 }
