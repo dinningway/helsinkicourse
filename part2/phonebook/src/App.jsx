49b26e46487dab4fc3115f0b68ecb1db9847158c
dinningway <avalonstar05@gmail.com>
2023-08-17 16:20:31 -0400

    Transfer to GitHub

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Name from "./components/Name";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState(""); //State for storing user-submitted name input
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      content: newName,
+      important: Math.random() < 0.5,
+      id: persons.length + 1,
+    };
+
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
 
   return (
-    <>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          {/*The event handler is called every time a change occurs in the input element.*/}
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {persons.map((person) => (
+          <Name key={person.id} name={person} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div>debug: {newName}</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
